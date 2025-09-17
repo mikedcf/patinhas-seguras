f93d4fbe6cc5a88a6d78cf230d69b9cb64931858
mikedcf <mikedcf74@gmail.com>
2025-09-17 09:17:50 -0300

    atualizado o cors

--- backend/javascript/server.js
+++ backend/javascript/server.js
@@ -34,7 +34,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5501',
+    origin: ['http://127.0.0.1:5501','https://mikedcf.github.io','http://localhost:5501'],
     credentials: true
 }));
 
