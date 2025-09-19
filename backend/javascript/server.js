6d6652843e99be2059f498291b6481920e815f68
mikedcf <mikedcf74@gmail.com>
2025-09-19 08:34:44 -0300

    teste de autenticacao

--- backend/javascript/server.js
+++ backend/javascript/server.js
@@ -46,8 +46,8 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24,
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax'
+        secure: true,
+        sameSite: 'none'
     }
 }))
 
