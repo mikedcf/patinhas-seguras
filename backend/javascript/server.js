338f4ecaf2120e76dd06d592d4bbd7fec0fc2c92
mikedcf <mikedcf74@gmail.com>
2025-09-21 23:10:57 -0300

    nova atualizacao do projeto melhorias no sistema de upload de imagens/videos com preupload e alteração no formulario de adocao.

--- backend/javascript/server.js
+++ backend/javascript/server.js
@@ -62,7 +62,7 @@ app.use(session({
 //         maxAge: 1000 * 60 * 60 * 24,
 //         httpOnly: true,
 //         secure: false,
-//         sameSite: 'lex'
+//         sameSite: 'lax'
 //     }
 // }))
 
